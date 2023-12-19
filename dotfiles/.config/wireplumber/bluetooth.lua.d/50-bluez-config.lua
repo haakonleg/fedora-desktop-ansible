174a6bb88295d39e651a20d738e7b29ddd102935
haakonleg <dev@haakon.me>
2023-12-19 21:03:55 +0100

    Update bluetooth config

--- dotfiles/.config/wireplumber/bluetooth.lua.d/50-bluez-config.lua
+++ dotfiles/.config/wireplumber/bluetooth.lua.d/50-bluez-config.lua
@@ -1,5 +1,6 @@
 bluez_monitor.properties = {
-  ["bluez5.enable-msbc"] = true,
   ["bluez5.enable-sbc-xq"] = true,
-  ["bluez5.codecs"] = "[ sbc sbc_xq aac ]",
+  ["bluez5.default.rate"] = 44100,
+  ["bluez5.codecs"] = "[ sbc_xq aac ldac aptx aptx_hd ]",
+  ["bluez5.a2dp.ldac.quality"] = "hq"
 }
